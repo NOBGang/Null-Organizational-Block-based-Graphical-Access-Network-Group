aca7f8264bc2f723b041a44fa1e8b9fd13626904
dr-ishigami-senku <nu522696@gmail.com>
2022-07-31 12:54:12 +0800

    alert message

--- contact.js
+++ contact.js
@@ -9,10 +9,12 @@ window.onload = function() {
                 emailjs.sendForm('service_6hag12f', 'template_j0v08gi', this)
                     .then(function() {
                         console.log('SUCCESS!');
-                        alert("Message Sent Successfully");
+                        alert("Message Sent Successfully! \nIf you do not see the email in a few minutes, check your “junk mail” folder or “spam” folder.");
+                        document.getElementById('contact-form').reset();
                     }, function(error) {
                         console.log('FAILED...', error);
-                        alert("Message Not Sent");
+                        alert("Message Not Sent!");
+                        document.getElementById('contact-form').reset();
                     });
             });
         }
